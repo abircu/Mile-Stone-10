6a2071ad30bc404b94bfb7a6886927df05ab7a31
abircu <mdabircse4873@gmail.com>
2023-10-11 00:19:48 +0530

    creating API and load explore

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const express=require('express')
+const express=require('express');
+const cors=require('cors');
+const data=require('./data.json');
 const app=express()
 const port=3000;
+app.use(cors());
 app.get('/',(req,res)=>{
     res.send('hello world!')
+})
+app.get('/data',(req,res)=>{
+ res.send(data);
+})
+app.get('/data/:id',(req,res)=>{
+    const id=parseInt(req.params.id)
+    const response = data.items;
+    console.log('i want to see request id',id)
+    const product= response.find(products=>products.id ===id) || {};
+    
+    res.send(product)
+
+    res.send()
 })
 app.listen(port,()=>{
     console.log(`apps listening on port:${port}`)
